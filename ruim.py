241bdcad64a6ddd5589d70e3482d4d99d720ffac
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2022-09-17 21:23:25 -0300

    Front setup finished

--- ruim.py
+++ ruim.py
@@ -1,5 +1,6 @@
 from tkinter import *
-from KML import KML
+from tkinter import ttk
+
 # cores
 co0 = "#FAE8E8" #rosa
 co1 = "#D9D9D9" #cinza
@@ -16,16 +17,26 @@ def criar_frame(quadro, row, column):
     return frame
 
 # criar widgets ###quadro é se seá colocado na janela ou em frame
-def criar_label(quadro, text, font, r, c):
-    Label(quadro, text=text, font=font, background=co1).grid(row=r, column=c, sticky = "w")
+def criar_label(quadro, text, font, r, c, name=""):
+    label = Label(quadro, text=text, font=font, background=co1, name=name)
+    label.grid(row=r, column=c, sticky = "w")
+    return label
 
-def criar_entry(quadro, font, r, c):
-    entrada = Entry(quadro, font = font, justify = "left")
-    entrada.grid(row=r, column=c, sticky = "w")
-    return entrada
+def criar_entry(quadro, font, r, c, name=""):
+    entry = Entry(quadro, font = font, justify = "left", name=name)
+    entry.grid(row=r, column=c, sticky = "w")
+    return entry
 
-def criar_button(quadro, text, font, r, c, command):
-    Button(quadro, text = text, font = font, height = 0, command = command ).grid(row=r, column=c, sticky = "w")
+def criar_button(quadro, text, font, r, c, command, name=""):
+    button = Button(quadro, text = text, font = font, height = 0, command = command, name=name)
+    button.grid(row=r, column=c, sticky = "w")
+    return button
+
+def get_child_by_name(parent, name):
+    for child in parent.winfo_children():
+        if child.name is name:
+            return child
+    return None
 
 # função que pegar o valor da caixa de entrada do "n° de sprints, ao apertar o button.
 # essa função também "abre um frame" para que as linhas referentes ao cadastro da sprint sejam encaixadas
@@ -46,31 +57,36 @@ def entry_sprint():
         criar_entry(frame_sprint, "Calibri, 10", i, 6)
 
 def entry_times():
-    valor = int(en_numtimes.get())
+    valor = get_entry_int(en_numtimes)
     for i in range(valor):
         row = i
         frame_time = criar_frame(frame_parent_times, row, 0)
-        lista_frame_time.append(frame_time)
+        # lista_frame_time.append(frame_time)
 
         frame_time_data = criar_frame(frame_time, 0, 0)
-        criar_label(frame_time_data, f"Time {i+1}", "Calibri, 10", row, 0)
-        criar_label(frame_time_data, "Quantidade de alunos", "Calibri, 10", row, 1)
-        criar_entry(frame_time_data, "Calibri, 10", row, 2)
+        criar_label(frame_time_data, f"Time {i+1}:",         "Calibri, 10", row, 0)
+        # criar_label(frame_time_data, "Nome", "Calibri, 10", row, 1)
+        criar_entry(frame_time_data,                         "Calibri, 10", row, 2)
+        criar_label(frame_time_data, "Quantidade de alunos", "Calibri, 10", row, 3)
+        criar_entry(frame_time_data,                         "Calibri, 10", row, 4, name="q_alunos")
         # lista.append(criar_button(frame_time_data, "Cadastrar", "Calibri, 10", i, 3, command = entry_alunos(en_numalunos)))
-        criar_button(frame_time_data, "Cadastrar", "Calibri, 10", row, 3, command = update_member_forms)
+        criar_button(frame_time_data, "Cadastrar",           "Calibri, 10", row, 5, command = update_member_forms)
 
 
 # Atualiza a tela para criar os formularios para cada membro de acordo com o numero de membros especificado em cada time
 def update_member_forms():
 
     # acessa a lista global de frames de time
-    global lista_frame_time
+    # global lista_frame_time
 
     # pra cada frame_time da lista
-    for ft_index, frame_time in enumerate(lista_frame_time):
+    for ft_index, frame_time in enumerate(frame_parent_times.winfo_children()):
 
+        e = frame_time.winfo_children()[0].children["q_alunos"]
+        print('e is "', e, '"')
 
-        num_alunos = get_entry_int(frame_time.winfo_children()[0].children['!entry'])
+        num_alunos = get_entry_int(e)
+        print('num_alunos ', num_alunos)
 
 
         members_parent = reset_or_create_frame(frame_time, 1, ft_index + 1)
@@ -79,6 +95,9 @@ def update_member_forms():
         # significa que não há mais nenhum passo a ser executado para o frame_time atual, continue o loop 
         if num_alunos < 1:
             continue 
+
+        num_alunos = (lambda x : 3 if x < 3 else (9 if x > 9 else x))(num_alunos)
+        print(num_alunos)
 
         # para cada aluno, cria um formulário de cadastro
         for i in range(num_alunos):
@@ -119,6 +138,7 @@ def get_entry_int (entry):
 
     # em caso de erro, retorne 0
     except:
+        print("erro")
         return 0
 
 
@@ -128,6 +148,11 @@ def criar_formulario_aluno (parent, row):
     criar_entry(parent,           "Calibri, 10", row, 1)
     criar_label(parent, "E-Mail", "Calibri, 10", row, 2)
     criar_entry(parent,           "Calibri, 10", row, 3)
+    criar_label(parent, "Função", "Calibri, 10", row, 4)
+    # TODO:
+    # from Roles.Role import *
+    # roles = ...
+    # criar_dropdown(parent, roles,"Calibri, 10", row, 5)
 
 
 
@@ -174,16 +199,55 @@ janela.geometry(res)  # aqui coloco o tamanho da tela, largura x altura
 # tentativa de dar numero de linhas e colunas para a tabela. Se deixo ativado, os labels ficam espalhados pela tela.
 # janela.rowconfigure([0,1,2,3], weight = 1, minsize=30)
 # janela.columnconfigure([0,1,2], weight = 1, minsize=30)
-janela.configure(background=co0)
+# janela.configure(background=co0)
+# janela.grid_rowconfigure(0, weight=1)
+# janela.grid_columnconfigure(0, weight=1)
 
-lista_frame_time = []
+# lista_frame_time = []
 
 # janela_header
 criar_label(janela, "Cadastro", "Calibri, 14", 0, 0)
 
+# frame_body = Frame(janela)
+# frame_body.pack(fill=BOTH, expand=1)
+
+
+# frame_body = criar_frame(janela, 1, 0)
+frame_body = Frame(janela)
+frame_body.grid(row=1, column=0)
+
+# frame_body.grid_rowconfigure(0, weight=1)
+# frame_body.grid_columnconfigure(0, weight=1)
+
+# canvas = Canvas(frame_body)
+# canvas.place(relwidth=1, relheight=1)
+# ttk.Scrollbar(frame_body, orient=VERTICAL, command=canvas.yview).place(relx=1, y=0, relheight=1)
+
+# frame_body.grid_rowconfigure(0, weight=1)
+# frame_body.grid_columnconfigure(0, weight=1)
+# frame_body.grid_propagate(False)
+
+# canvas = Canvas(frame_body, bg="yellow")
+# canvas = Canvas(frame_body)
+# canvas.grid(row=0, column=0, sticky="nsew")
+# photoScroll = Scrollbar(frame_body, orient=VERTICAL)
+# photoScroll.config(command=canvas.yview)
+# canvas.config(yscrollcommand=photoScroll.set)
+# photoScroll.grid(row=0, column=1, sticky="ns")
+# def update_scrollregion(event):
+#     canvas.configure(scrollregion=canvas.bbox("all"))
+
+# canvas.bind("<Configure>", update_scrollregion)
+
 # cria a seção de sprints
-frame_section0 = criar_frame(janela, 1, 0)
+frame_section0 = criar_frame(frame_body, 1, 0)
+# frame_section0.grid_rowconfigure(0, weight=1)
+# frame_section0.grid_columnconfigure(0, weight=1)
+
 frame_sprints = criar_frame(frame_section0, 0, 0)
+# frame_sprints.grid_rowconfigure(0, weight=1)
+# frame_sprints.grid_columnconfigure(0, weight=1)
+
 
 # título
 criar_label(frame_sprints, "Sprints", "Calibri, 12", 0, 0)
@@ -195,7 +259,11 @@ criar_button(frame_sprints, "Cadastrar", "Calibri, 10", 1, 2, command = entry_sp
 
 # data: list-wrapper
 frame_parent_sprints = criar_frame(frame_section0, 2, 0)
+# frame_parent_sprints.grid_rowconfigure(0, weight=1)
+# frame_parent_sprints.grid_columnconfigure(0, weight=1)
 
+
+# TODO:
 # Cria uma 'section' de acordo com a seguinte hierarquia:
 #           section
 #           |   header
@@ -207,14 +275,19 @@ frame_parent_sprints = criar_frame(frame_section0, 2, 0)
 #      'header' contém 'title' e 'header_data'
 #
 # '...._data' corresponde a uma lista de parametros a serem convertidos em widgets
-
-def create_section (window, row, title, header_data, ):
-    frame_section = criar_frame(window, row, 0)
+# def create_section (window, row, title, header_data, ):
+#     frame_section = criar_frame(window, row, 0)
 
 
 # cria a seção de times
-frame_section1 = criar_frame(janela, 2, 0)
+frame_section1 = criar_frame(frame_body, 2, 0)
+# frame_section1.grid_rowconfigure(0, weight=1)
+# frame_section1.grid_columnconfigure(0, weight=1)
+
 times_header = criar_frame(frame_section1, 0, 0)
+# times_header.grid_rowconfigure(0, weight=1)
+# times_header.grid_columnconfigure(0, weight=1)
+
 
 # título
 criar_label(times_header, "Times", "Calibri, 12", 0, 0)
@@ -226,7 +299,24 @@ criar_button(times_header, "Cadastrar", "Calibri, 10", 1, 2, command = entry_tim
 
 # data: list-wrapper
 frame_parent_times = criar_frame(frame_section1, 2, 0)
+# frame_parent_times.grid_rowconfigure(0, weight=1)
+# frame_parent_times.grid_columnconfigure(0, weight=1)
 
+
+def confirmar_cadastros():
+    # get sprints
+    # for each sprint:
+    #   save to sprints.csv
+
+    # get teams
+    #   name
+    #   save team
+    #   for each member:
+    #       register(member)
+
+    """"""
+
+Button(janela, text="Confirmar Cadastros", font="Calibri, 14", command=confirmar_cadastros).place(rely=0, relx=1.0, x=0, y=0, anchor=NE)
 
 janela.mainloop()
 
