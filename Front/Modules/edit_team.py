6d21b45f999c461171fbc86465c67b6e54f0f557
Paulo Granthon <paulo.granthon@fatec.sp.gov.br>
2022-10-07 13:10:06 -0300

    Composition update

--- Front/Modules/edit_team.py
+++ Front/Modules/edit_team.py
@@ -17,18 +17,22 @@ REQUIRED_PERMISSIONS_VIEW = [None]
 def run(frame_parent):
 
     # cria o frame do módulo
-    module_frame = Frame(frame_parent, background=co0)
-    module_frame.grid(row=0, column=0, sticky="nwes")
+    module_frame = Frame(frame_parent, padx=2, pady=2, bg='magenta')
+    # module_frame.rowconfigure(0, minsize = 0, weight = 1)
+    module_frame.columnconfigure(0, minsize = 0, weight = 1)
+    module_frame.grid(row=0, column=0, sticky="news")
 
     # section 0
-    frame_title = Frame(frame_parent)
+    frame_title = Frame(module_frame, padx=2, pady=2, bg='blue')
     frame_title.grid(row=0, column=0, sticky='we')
 
     Label(frame_title, text="Editar", font='Calibri, 20').grid(row=0, column=0)
 
     # section 1
-    frame_teams = Frame(frame_parent)
-    frame_teams.grid(row=1, column=0, sticky="we")
+    frame_teams = Frame(module_frame, padx=2, pady=2, bg='cyan')
+    frame_teams.rowconfigure(1, minsize = 0, weight = 1)
+    frame_teams.columnconfigure(0, minsize = 0, weight = 1)
+    frame_teams.grid(row=1, column=0, sticky="ew")
 
     from CSV.CSVHandler import find_data_list_by_field_value_csv
     from Users.Authentication import CURRENT_USER
@@ -46,8 +50,9 @@ def run(frame_parent):
         print(f'i: [{i}]: time "{time_data}"')
 
         # cria o frame do time
-        frame_team = Frame(frame_teams)
-        frame_team.grid(row=i, column=0, sticky="we")
+        frame_team = Frame(frame_teams, padx=2, pady=2, bg="yellow")
+        frame_team.columnconfigure(0, minsize = 0, weight = 1)
+        frame_team.grid(row=i, column=0, sticky="ew")
 
         # coloca o nome do time
         Label(frame_team, text=get_team_name(int(time_data['id'])), font='Calibri, 16').grid(row=0, column=0)
@@ -61,11 +66,31 @@ def run(frame_parent):
             print(f'member: {member_data}')
 
             # cria um frame para o membro dentro do frame_time
-            frame_member = Frame(frame_team)
+            frame_member = Frame(frame_team, padx=2, pady=2)
+            frame_member.columnconfigure(0, minsize = 0, weight = 1)
             frame_member.grid(row=j+1, column=0, sticky="we")
 
             # coloca o nome do membro
-            Label(frame_member, text=member_data['name'], font='Calibri, 12').grid(row=0, column=0)
+            frame_member_name = Frame(frame_team, padx=2, pady=2)
+            # frame_member_name.columnconfigure(0, minsize = 0, weight = 1)
+            frame_member_name.grid(row=j+1, column=0, sticky="w")
+            Label(frame_member_name, text=member_data['name'], font='Calibri, 12', justify='left', bg='green', padx=2, pady=2).grid(row=0, column=0, sticky="ew")
+
+            # cria um frame parent para as ações
+            frame_actions = Frame(frame_member)
+            frame_actions.columnconfigure(0, minsize = 0, weight = 1)
+            frame_actions.grid(row=0, column=1, sticky="w")
+
+            # cria o frame e dropdown de role dentro do ações
+            frame_dropdown = Frame(frame_actions)
+            frame_dropdown.grid(row=0, column=0)
+            Label(frame_dropdown, text='dropdown', font='Calibri, 12', padx=8, pady=2, bg='blue').grid(row=0, column=0)
+
+            # cria o frame e button remover dentro do ações
+            frame_remover = Frame(frame_actions)
+            frame_remover.grid(row=0, column=1)
+            Label(frame_remover, text='remover', font='Calibri, 12', padx=8, pady=2, bg='red').grid(row=0, column=0)
+
 
 
 
